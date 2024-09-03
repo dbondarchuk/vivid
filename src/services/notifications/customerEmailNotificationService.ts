06a492168ab00e92c5abc12d0e279c558df61c31
Dmytro Bondarchuk <dmitriy.bondarchuk@outlook.com>
2024-09-03 10:45:34 -0400

    Fix confirm

--- src/services/notifications/customerEmailNotificationService.ts
+++ src/services/notifications/customerEmailNotificationService.ts
@@ -64,7 +64,7 @@ export class CustomerEmailNotificationService extends IEmailNotificationService 
       "PUBLISH"
     );
 
-    const { subject, body } = booking.email.templates.declined;
+    const { subject, body } = booking.email.templates.confirmed;
 
     await this.sendEmail({
       to: appointment.fields.email,
