6bfa10ed838c8a58bad5426685085f8cab91656e
Dmytro Bondarchuk <dmitriy.bondarchuk@outlook.com>
2024-09-23 09:37:18 -0400

    Add assets route

---
+++ src/app/assets/[[...slug]]/route.ts
@@ -0,0 +1,63 @@
+import { createReadStream, existsSync } from "fs";
+import fs from "fs/promises";
+import { mimeType } from "mime-type/with-db";
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import { ReadableOptions } from "stream";
+
+/**
+ * Return a stream from the disk
+ * @param {string} path - The location of the file
+ * @param {ReadableOptions} options - The streamable options for the stream (ie how big are the chunks, start, end, etc).
+ * @returns {ReadableStream} A readable stream of the file
+ */
+function streamFile(
+  path: string,
+  options?: ReadableOptions
+): ReadableStream<Uint8Array> {
+  const downloadStream = createReadStream(path, options);
+
+  return new ReadableStream({
+    start(controller) {
+      downloadStream.on("data", (chunk: Buffer) =>
+        controller.enqueue(new Uint8Array(chunk))
+      );
+      downloadStream.on("end", () => controller.close());
+      downloadStream.on("error", (error: NodeJS.ErrnoException) =>
+        controller.error(error)
+      );
+    },
+    cancel() {
+      downloadStream.destroy();
+    },
+  });
+}
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string[] } }
+): Promise<NextResponse> {
+  const file = path.join(process.cwd(), "public", "upload", ...params.slug);
+
+  if (!existsSync(file)) {
+    console.warn(`File ${params.slug} not found`);
+    return notFound();
+  }
+
+  const filename = path.basename(file);
+  const contentType = (mimeType.contentType(filename) as string) || "";
+
+  const stats = await fs.stat(file); // Get the file size
+  const data: ReadableStream<Uint8Array> = streamFile(file); // Stream the file with a 1kb chunk
+  const res = new NextResponse(data, {
+    status: 200,
+    headers: new Headers({
+      //Headers
+      "content-disposition": `attachment; filename=${filename}`, //State that this is a file attachment
+      "content-type": contentType,
+      "content-length": stats.size + "",
+    }),
+  });
+
+  return res;
+}
