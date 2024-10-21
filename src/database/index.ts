97b232bdcc5d072dfbf49ed1df8528dd80013cba
Dmytro Bondarchuk <dmitriy.bondarchuk@outlook.com>
2024-10-21 08:24:45 -0400

    Attempt fix mongo connections

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,31 @@
 import { MongoClient } from "mongodb";
 import { env } from "process";
-import { cache } from "react";
 
-export const getDbClient = cache(() => new MongoClient(env["MONGODB_URI"]!));
+if (!env["MONGODB_URI"]) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = env["MONGODB_URI"];
+const options = { appName: "vivid" };
+
+let client: MongoClient;
+
+if (env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
+
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  client = globalWithMongo._mongoClient;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+}
 
 export const getDbConnection = async () => {
-  return getDbClient().db();
+  return client.db();
 };
