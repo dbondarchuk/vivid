7547fd8c9369bce5379ed9a2458f34c54e9d1f90
Dmytro Bondarchuk <dmitriy.bondarchuk@outlook.com>
2025-02-08 22:46:57 -0500

    Remove dynamic

--- apps/web/src/app/admin/api/assets/route.ts
+++ apps/web/src/app/admin/api/assets/route.ts
@@ -1,8 +1,6 @@
 import { ServicesContainer } from "@vivid/services";
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const mimeType = searchParams.get("mimeType");
